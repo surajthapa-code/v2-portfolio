135cf9539eb48b5f6071ee188b5cc0cdd3abfc8a
Suraj Thapa <surajthapa7787@gmail.com>
2026-07-13 08:23:43 +0530

    skill page and main page setup

---
+++ v2-portfolio/src/components/Skills.tsx
@@ -0,0 +1,12 @@
+function Skills() {
+  return (
+    <div>
+      <header className="flex bg-zinc-950 h-13 text-white justify-center items-center font-serif text-3xl font-medium ">
+        <p>Skills</p>
+      </header>
+      <section></section>
+    </div>
+  );
+}
+
+export default Skills;
--- v2-portfolio/src/pages/MainPage.tsx
+++ v2-portfolio/src/pages/MainPage.tsx
@@ -1,14 +1,34 @@
 import Navbar from "@/components/Navbar";
+import Skills from "@/components/Skills";
 import React from "react";
 
 function MainPage() {
   return (
     <div className="w-full overflow-hidden">
       <Navbar />
-      <div className="w-screen">
-        {/* <h2 className="">Suraj</h2> */}
-        <img className="w-full h-full object-cover" src="/animeWP.jpg" alt="" />
+      <div className="w-screen relative h-170">
+        <img
+          className="w-screen h-full object-cover"
+          src="/animeWP.jpg"
+          alt="Background"
+        />
+        <div className="absolute inset-0 flex items-center justify-center bg-black/40">
+          <div className="text-center text-white px-4">
+            <div className="flex justify-center gap-3 ">
+              <h2 className="text-4xl font-bold font-serif sm:text-5xl">
+                Suraj
+              </h2>
+              <h2 className="text-4xl text-amber-400 font-bold font-serif sm:text-5xl">
+                Thapa
+              </h2>
+            </div>
+            <p className="mt-3 text-sm sm:text-base text-gray-200">
+              CS UnderGrad | Software Development Engineer 
+            </p>
+          </div>
+        </div>
       </div>
+      <Skills />
     </div>
   );
 }
